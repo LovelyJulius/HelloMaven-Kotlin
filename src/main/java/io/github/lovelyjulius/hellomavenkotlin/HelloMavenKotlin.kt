488435ac8664152b87847a87020555adff880a07
LovelyJulius <63322153+LovelyJulius@users.noreply.github.com>
2022-05-04 13:09:11 +0800

    更新。

---
+++ src/main/java/io/github/lovelyjulius/hellomavenkotlin/HelloMavenKotlin.kt
@@ -0,0 +1,10 @@
+package io.github.lovelyjulius.hellomavenkotlin
+
+import java.sql.DriverManager.println
+
+object HelloMavenKotlin {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        kotlin.io.println("Hello Maven(Kotlin)!")
+    }
+}
